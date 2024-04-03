bfca170ad67210d45d5d5e256cf9155bd5634f15
XFinity <x.finity777@gmail.com>
2024-04-04 02:26:09 +0300

    add 4th hw

--- 04_Iterators/main.py
+++ 04_Iterators/main.py
@@ -1,15 +1,26 @@
 class FlatIterator:
 
     def __init__(self, list_of_list):
-        ...
+        self.list_of_list = list_of_list
+        
 
     def __iter__(self):
-        ...
+        self.list = []
+        # self.list = self._pop_list()
         return self
+    
+    def _pop_list(self):
+        return self.list_of_list.pop(0)
 
     def __next__(self):
-        ...
+        try:
+            if not self.list: self.list = self._pop_list()
+        except IndexError:
+            raise StopIteration
+        item = self.list.pop(0)
+
         return item
+
 
 
 def test_1():
@@ -31,4 +42,19 @@ def test_1():
 
 
 if __name__ == '__main__':
-    test_1()
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+    for i in range(3):
+        print(list(FlatIterator(list_of_lists_1)))
+    # if list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]:
+    #     print(list(FlatIterator(list_of_lists_1)))
+    #     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None], 'Not OK!'
+    #     print('OK')
+    
+    # for item in FlatIterator(list_of_lists_1):
+    #     print(item)
+    # print(list(FlatIterator(list_of_lists_1)))
+    # test_1()
